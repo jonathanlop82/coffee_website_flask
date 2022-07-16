4781ca77d09289c028dc243c5cb91589410230c4
Jonathan Lopez <jonathanlop82@gmail.com>
2022-07-15 21:33:46 -0500

    Add records and done

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 import random
 
@@ -37,6 +37,62 @@ def index():
     cafes = db.session.query(Cafe).all()
     return render_template("index.html", cafes=cafes)
 
+@app.route("/add", methods=['GET', 'POST'])
+def add():
+    if request.method == "POST":
+        name = request.form.get('name')
+        url_map = request.form.get('url_map')
+        url_image = request.form.get('url_image')
+        location = request.form.get('location')
+        has_sockets = request.form.get('has_sockets')
+        has_toilet = request.form.get('has_toilet')
+        can_take_calls = request.form.get('can_take_calls')
+        has_wifi = request.form.get('has_wifi')
+        seats = request.form.get('seats')
+        coffee_price = request.form.get('coffee_price')
+
+        if has_sockets == "has_sockets":
+            has_sockets = True
+        else:
+            has_sockets = False
+
+        if has_toilet == "has_toilet":
+            has_toilet = True
+        else:
+            has_toilet = False
+
+        if can_take_calls == "can_take_calls":
+            can_take_calls = True
+        else:
+            can_take_calls = False
+
+        if has_wifi == "has_wifi":
+            has_wifi = True
+        else:
+            has_wifi = False
+
+        new_coffee = Cafe(
+            name = name,
+            map_url = url_map,
+            img_url = url_image,
+            location = location,
+            seats = seats,
+            has_toilet = has_toilet,
+            has_wifi = has_wifi,
+            has_sockets = has_sockets,
+            can_take_calls = can_take_calls,
+            coffee_price = coffee_price
+        )
+
+        db.session.add(new_coffee)
+        db.session.commit()
+
+        return redirect(url_for("index"))
+
+    return render_template("add.html")
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        debug=True)
